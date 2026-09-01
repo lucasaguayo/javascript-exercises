479e74a469b2f2361068f5eccb4657021ba453de
Lucas Aguayo <lucasaguayorr@gmail.com>
2026-09-01 09:12:16 -0300

    Completed the factorial exercise.

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,10 @@
-const factorial = function() {
-
+const factorial = function(n) {
+  if (n === 0) {
+    return 1;
+  } else if (typeof n !== "number" || n < 0 || !Number.isInteger(n)){
+    return undefined;
+  }
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
