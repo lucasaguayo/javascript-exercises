4580a8ba43ae031d0136cbd1b8f338f9a1e117ad
Lucas Aguayo <lucasaguayorr@gmail.com>
2026-09-03 10:16:52 -0300

    Completed the totalIntegers exercise.

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,26 @@
-const totalIntegers = function() {
+// checks if the value given is an object.
+const isObject = (value) => typeof value === "object" && value !== null;
+
+const totalIntegers = function(input) {
+  let count = 0;
+
+  if (!isObject(input)) return undefined;
+
+  const elements = Object.values(input)
   
+  for (const element of elements) {
+    if (Number.isInteger(element)) {
+      count++;
+    } else if (isObject(element)) {
+      count += totalIntegers(element)
+    };
+  }
+
+  return count;
 };
   
+// totalIntegers([[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]]); // returns 7
+// totalIntegers({ a: 1, b: { a: [5, 10], b: 11 } }); // returns 4
+
 // Do not edit below this line
 module.exports = totalIntegers;
